bfc99a5cb4e0759ee4384a32c95a0c2e701540b2
nicoacosta.eth <nicoacosta8@gmail.com>
2025-10-29 03:25:09 -0300

    init nextra

---
+++ app/layout.jsx
@@ -0,0 +1,53 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+ 
+export const metadata = {
+  title: 'Uniswap v4 Hooks',
+  description: 'Documentation for Uniswap v4 Hooks',
+  icons: {
+    icon: '/favicon.svg',
+  },
+}
+ 
+const navbar = (
+  <Navbar
+    logo={<b>Uniswap v4 Hooks</b>}
+  />
+)
+const footer = (
+  <Footer>
+    🦄 🤍
+  </Footer>
+)
+ 
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          footer={footer}
+          // ... Your additional layout options
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
