63f1aacf35c883b086e83a78ee4bc2fa1022ddbd
nicoacosta.eth <nicoacosta8@gmail.com>
2025-10-29 19:48:54 -0300

    feat(pages): init new structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
+import { Funnel_Display, Funnel_Sans } from 'next/font/google'
 import 'nextra-theme-docs/style.css'
+
+const funnelDisplay = Funnel_Display({ weight: ['400', '500', '600', '700'], subsets: ['latin'] })
+const funnelSans = Funnel_Sans({ weight: ['400', '500', '600', '700'], subsets: ['latin'] })
  
 export const metadata = {
   title: 'Uniswap v4 Hooks',
@@ -13,7 +17,7 @@ export const metadata = {
  
 const navbar = (
   <Navbar
-    logo={<b>Uniswap v4 Hooks</b>}
+    logo={<><b className={funnelDisplay.className} style={{ fontSize: '2rem', color: '#f50db4' }} >hooks</b><b className={funnelDisplay.className} style={{ fontSize: '2rem' }} >.club</b></>}
   />
 )
 const footer = (
@@ -37,7 +41,7 @@ export default async function RootLayout({ children }) {
       >
         {/* Your additional tags should be passed as `children` of `<Head>` element */}
       </Head>
-      <body>
+      <body className={funnelSans.className}>
         <Layout
           navbar={navbar}
           pageMap={await getPageMap()}
